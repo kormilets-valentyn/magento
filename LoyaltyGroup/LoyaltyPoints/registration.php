0f06507a9fa5bd227271546f7a69123d1e18dfca
Valentyn <v.kormilets@gmail.com>
2020-04-07 15:56:56 +0300

    some change

--- LoyaltyGroup/LoyaltyPoints/registration.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Magento\Framework\Component\ComponentRegistrar;
-
-$registrar = new ComponentRegistrar();
-if ($registrar->getPath(ComponentRegistrar::MODULE, 'LoyaltyGroup_LoyaltyPoints') === null) {
-    ComponentRegistrar::register(ComponentRegistrar::MODULE, 'LoyaltyGroup_LoyaltyPoints', __DIR__);
-}
